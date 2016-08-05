a42adb48d6639ff4f20f3c14f1213fc01f8e87a1
Daniel Schmidt <danielmschmidt92@gmail.com>
2016-08-05 22:56:52 +0200

    added scroll and back button to list

--- app/containers/List.js
+++ app/containers/List.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import ListItem from '../components/ListItem';
 import jargons from '../utils/jargons';
-
+import styles from './List.css';
 export default class HomePage extends Component {
   render() {
     return (
       <div>
-        {Object.keys(jargons).map(key => (
-          <ListItem headline={key} content={jargons[key]} />
-        ))}
+        <div className={styles.backButton}>
+          <Link to="/">
+            <i className="fa fa-arrow-left fa-3x" />
+          </Link>
+        </div>
+        <div className={styles.content}>
+          {Object.keys(jargons).map(key => (
+            <ListItem headline={key} content={jargons[key]} />
+          ))}
+        </div>
       </div>
     );
   }
